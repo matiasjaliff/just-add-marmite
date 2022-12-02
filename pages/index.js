0bccd5c75a655c25ac8f920052f6d00bef2e258b
Matias Jaliff <matiasjaliff@gmail.com>
2022-12-02 09:41:04 -0300

    try to fix isr of index

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,29 @@
-import { createClient } from "contentful";
-
-import RecipeCard from "../components/RecipeCard";
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
 
 export async function getStaticProps() {
+
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  });
-  const res = await client.getEntries({
-    content_type: "recipe",
-  });
+  })
+
+  const res = await client.getEntries({ content_type: "recipe" })
+
   return {
     props: {
       recipes: res.items,
     },
-    revalidate: 1,
-  };
+    revalidate: 1
+  }
 }
 
 export default function Recipes({ recipes }) {
+  console.log(recipes)
+
   return (
     <div className="recipe-list">
-      {recipes.map((recipe) => (
+      {recipes.map(recipe => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
 
@@ -33,5 +35,5 @@ export default function Recipes({ recipes }) {
         }
       `}</style>
     </div>
-  );
+  )
 }
