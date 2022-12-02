c6d3163316d2d36a4fcf16bd6182c6480647d9fc
Matias Jaliff <matiasjaliff@gmail.com>
2022-12-02 10:53:34 -0300

    add redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -34,6 +34,15 @@ export async function getStaticProps({ params }) {
     "fields.slug": params.slug,
   });
 
+  if (!items.length) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: { recipe: items[0] },
     revalidate: 1,
@@ -42,11 +51,8 @@ export async function getStaticProps({ params }) {
 
 export default function RecipeDetails({ recipe }) {
   if (!recipe) {
-    console.log("Sin receta");
     return <Skeleton />;
   }
-
-  console.log("Con receta");
 
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
